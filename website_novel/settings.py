6fb9608b692c9fd772119c1266f56226982d045a
Yannick AGOSSOU <fredyann@live.com>
2024-07-26 10:53:26 +0100

    edit allowed host in settings.py

--- website_novel/settings.py
+++ website_novel/settings.py
@@ -376,9 +376,10 @@ DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 #     ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # ALLOWED_HOSTS = ['novel-app-2yrg.onrender.com']
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
-if not ALLOWED_HOSTS:
-    ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'novel-app-2yrg.onrender.com']
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
+# if not ALLOWED_HOSTS:
+#     ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'novel-app-2yrg.onrender.com']
 # ALLOWED_HOSTS = []
 
 # Application definition
