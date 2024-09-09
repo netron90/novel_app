a7acf3949b729546682829e6e6f0e5f9863a871a
Yannick AGOSSOU <fredyann@live.com>
2024-09-09 16:59:03 +0100

    add api code to get all town and novel categories in the database.

--- novel/urls.py
+++ novel/urls.py
@@ -12,6 +12,6 @@ urlpatterns=[
     path("<int:town_id>/get-categories", views.get_categories, name="categories"),
     path('<int:country_id>/<int:town_id>/<int:category_id>/get-novels', views.get_novels, name='get-novels'),
     path("api/get-countries", views.get_countries, name="allcountries"),
-    path("api/get-towns", views.get_towns, name="alltowns"),
-    path("api/get-categories", views.get_categories, name="allcategories"),
+    path("api/get-towns/<int:country_id>", views.get_towns, name="alltowns"),
+    path("api/get-categories/<int:town_id>", views.get_categories, name="allcategories"),
 ]
